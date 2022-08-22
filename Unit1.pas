ea8c73defe1ebd7817635f1e69f4330194711397
cvghivebrain <66194501+cvghivebrain@users.noreply.github.com>
2022-08-22 22:08:56 +0100

    Moved function to separate pas file

--- Unit1.pas
+++ Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, StrUtils;
+  Dialogs, StdCtrls, StrUtils, ExplodeFunc;
 
 type
   TForm1 = class(TForm)
@@ -16,7 +16,6 @@ type
     procedure btnGoClick(Sender: TObject);
   private
     { Private declarations }
-    function Explode(s, d: string; n: integer): string;
   public
     { Public declarations }
   end;
@@ -29,35 +28,6 @@ implementation
 {$R *.dfm}
 
 { TForm1 }
-
-{ Replicate MediaWiki's "explode" string function. }
-function TForm1.Explode(s, d: string; n: integer): string;
-var n2: integer;
-begin
-  if (AnsiPos(d,s) = 0) and ((n = 0) or (n = -1)) then result := s // Output full string if delimiter not found.
-  else
-    begin
-    if n > -1 then // Check for negative substring.
-      begin
-      s := s+d;
-      n2 := n;
-      end
-    else
-      begin
-      d := AnsiReverseString(d);
-      s := AnsiReverseString(s)+d; // Reverse string for negative.
-      n2 := Abs(n)-1;
-      end;
-    while n2 > 0 do
-      begin
-      Delete(s,1,AnsiPos(d,s)+Length(d)-1); // Trim earlier substrings and delimiters.
-      dec(n2);
-      end;
-    Delete(s,AnsiPos(d,s),Length(s)-AnsiPos(d,s)+1); // Trim later substrings and delimiters.
-    if n < 0 then s := AnsiReverseString(s); // Un-reverse string if negative.
-    result := s;
-    end;
-end;
 
 procedure TForm1.btnGoClick(Sender: TObject);
 begin
